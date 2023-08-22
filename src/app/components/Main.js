8e572016779a2d6545359ea073ce239cc62efd05
Muhammadasad9554 <muhammadasad@gordiansol.com>
2023-08-22 14:07:47 +0500

    Added my project

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,15 +1,128 @@
-import React, { useState } from 'react';
+// import React, { useState } from "react";
 
+// const Main = ({ onLogout }) => {
+//   const [items, setItems] = useState([]);
+//   const [newItem, setNewItem] = useState("");
+//   const [color, setColor] = useState("black");
+
+//   const handleAddItem = () => {
+//     if (newItem.trim() !== "") {
+//       setItems([...items, { text: newItem, color }]);
+//       setNewItem("");
+//     }
+//   };
+
+//   const handleRemoveItem = (index) => {
+//     const updatedItems = items.filter((_, i) => i !== index);
+//     setItems(updatedItems);
+//   };
+
+//   return (
+//     <div className="container">
+//       <h1 className="app-title">Reminder App</h1>
+//       <button className="logout" onClick={onLogout}>
+//         LOGOUT
+//       </button>
+//       <div className="input-container">
+//       <input
+//   className="text-input"
+//   type="text"
+//   value={newItem}
+//   onChange={(e) => setNewItem(e.target.value)}
+//   placeholder="Enter text"
+// />
+
+//         <button className="add-button" onClick={handleAddItem}>
+//           +
+//         </button>
+//         <input
+//           className="color-picker"
+//           type="color"
+//           value={color}
+//           onChange={(e) => setColor(e.target.value)}
+//         />
+//       </div>
+//       <ul className="item-list">
+//         {items.map((item, index) => (
+//           <li key={index} className="list-item">
+//             <span className="list-text" style={{ color: item.color }}>
+//               {item.text}
+//             </span>
+//             <button
+//               className="remove-button"
+//               onClick={() => handleRemoveItem(index)}
+//             >
+//               Remove
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Main;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from "react";
 
 const Main = ({ onLogout }) => {
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('');
-  const [color, setColor] = useState('black');
+  const [editingIndex, setEditingIndex] = useState(null); // Track the index of the item being edited
+
+  const [newItem, setNewItem] = useState("");
+  const [color, setColor] = useState("black");
 
   const handleAddItem = () => {
-    if (newItem.trim() !== '') {
-      setItems([...items, { text: newItem, color }]);
-      setNewItem('');
+    if (newItem.trim() !== "") {
+      const currentTime = new Date().toLocaleTimeString();
+      setItems([...items, { text: newItem, time: currentTime, color }]);
+      setNewItem("");
     }
   };
 
@@ -18,33 +131,79 @@ const Main = ({ onLogout }) => {
     setItems(updatedItems);
   };
 
+  const handleEditItem = (index) => {
+    setEditingIndex(index);
+    setNewItem(items[index].text); // Set the input value to the current item's text
+  };
+
+  const handleSaveEdit = () => {
+    if (editingIndex !== null) {
+      const updatedItems = [...items];
+      updatedItems[editingIndex].text = newItem;
+      setItems(updatedItems);
+      setEditingIndex(null);
+      setNewItem("");
+    }
+  };
+
   return (
-    <div className='container'>
-      <h1 className='app-title'>Reminder App</h1>
-      <button className='logout' onClick={onLogout}>
+    <div className="container">
+      <h1 className="app-title">Reminder App</h1>
+      <button className="logout" onClick={onLogout}>
         LOGOUT
       </button>
-      <div className='input-container'>
+      <div className="input-container">
         <input
-          className='text-input'
-          type='text'
+          className="text-input"
+          type="text"
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
-          placeholder='Enter text'
+          placeholder="Enter text"
         />
-        <button className='add-button' onClick={handleAddItem}>+</button>
+        <button className="add-button" onClick={handleAddItem}>
+          +
+        </button>
         <input
-          className='color-picker'
-          type='color'
+          className="color-picker"
+          type="color"
           value={color}
           onChange={(e) => setColor(e.target.value)}
         />
       </div>
-      <ul className='item-list'>
+      <ul className="item-list">
         {items.map((item, index) => (
-          <li key={index} className='list-item'>
-            <span className='list-text' style={{ color: item.color }}>{item.text}</span>
-            <button className='remove-button' onClick={() => handleRemoveItem(index)}>Remove</button>
+          <li key={index} className="list-item">
+            <div className="item-content">
+              {editingIndex === index ? (
+                <input
+                  type="text"
+                  value={newItem}
+                  onChange={(e) => setNewItem(e.target.value)}
+                />
+              ) : (
+                <span className="list-text" style={{ color: item.color }}>
+                  {item.text}
+                </span>
+              )}
+              <span className="list-time">{item.time}</span>
+            </div>
+            <div className="button-container">
+              {editingIndex === index ? (
+                <button className="save-button" onClick={handleSaveEdit}>
+                  Save
+                </button>
+              ) : (
+                <button className="edit-button" onClick={() => handleEditItem(index)}>
+                  Edit
+                </button>
+              )}
+              <button
+                className="remove-button"
+                onClick={() => handleRemoveItem(index)}
+              >
+                Remove
+              </button>
+            </div>
           </li>
         ))}
       </ul>
